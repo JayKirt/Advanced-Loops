d957eb72bd523e017dd0633c845ae26679cdf1f2
Jay Kirtlan <jaykirtlan@hotmail.com>
2023-11-30 14:54:29 +0000

    original loops

---
+++ AdvancedLoops.js
@@ -0,0 +1,9 @@
+// there are for/while/do/forEach loops but there are also two other types of loops as below
+
+const basket = [ 'apples','oranges', 'grapes']
+
+// Original way 
+for (let i=0; i < basket.length; i++ ){
+    console.log(basket[i])
+};
+
